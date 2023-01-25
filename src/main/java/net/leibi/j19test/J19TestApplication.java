87fad0888f5cd166ba57aed9fe6211ccb8438b30
Andreas Leibrock <andreas.leibrock@infrontfinance.com>
2023-01-25 14:40:30 +0100

    initial

---
+++ src/main/java/net/leibi/j19test/J19TestApplication.java
@@ -0,0 +1,13 @@
+package net.leibi.j19test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J19TestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(J19TestApplication.class, args);
+  }
+
+}
